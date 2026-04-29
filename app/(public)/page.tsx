b9868d46a723005e86dbac184dbf0b3b01397daf
oreocapybara <71880256+oreocapybara@users.noreply.github.com>
2026-04-29 22:57:36 +0800

    feat: update layout and page components, enhance button styles, and adjust Tailwind config

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,7 +2,7 @@ import Map from "@/components/map/Map";
 
 export default function Page() {
 	return (
-		<main className="min-h-screen w-full">
+		<main className="flex min-h-screen w-screen">
 			<Map />
 		</main>
 	);
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -5,52 +5,71 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-  {
-    variants: {
-      variant: {
-        default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  },
+	"inline-flex items-center justify-center font-body font-bold gap-[6px] whitespace-nowrap rounded-2xl  text-m  transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+	{
+		variants: {
+			variant: {
+				default:
+					"bg-surface-brand shadow hover:bg-surface-brand-hover active:bg-surface-brand-pressed text-content-inverse-primary",
+				secondary: 
+					"bg-surface-primary border-stroke-primary border-[1px] text-content-brand shadow-sm hover:bg-surface-hover hover:text-content-link active:bg-surface-pressed active:text-content-link-pressed",
+				tertiary:
+					"bg-surface-primary text-content-brand hover:bg-surface-hover hover:text-content-link-hover active:bg-surface-pressed",
+				mono: "bg-surface-primary text-content-primary hover:bg-surface-hover active:bg-surface-pressed",
+				link: "text-content-link underline-offset-4 underline hover:text-content-link-hover active:text-content-link-pressed",
+			},
+			size: {
+				default: "w-full px-6 py-2",
+				sm: "w-fill px-4 py-1",
+				icon: "p-2",
+			},
+			squared: {
+				true: "rounded",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+			size: "default",
+		},
+	},
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
+	extends
+		React.ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonVariants> {
+	asChild?: boolean;
+	leadingIcon?: React.ReactNode;
+	trailingIcon?: React.ReactNode;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    );
-  },
+	(
+		{
+			className,
+			variant,
+			size,
+			squared,
+			leadingIcon,
+			trailingIcon,
+			asChild = false,
+			...props
+		},
+		ref,
+	) => {
+		const Comp = asChild ? Slot : "button";
+		return (
+			<Comp
+				className={cn(buttonVariants({ variant, size, squared, className }))}
+				ref={ref}
+				{...props}
+			>
+				{leadingIcon}
+				{props.children}
+				{trailingIcon}
+			</Comp>
+		);
+	},
 );
 Button.displayName = "Button";
 
